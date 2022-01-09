482101db7be22a14a58e7775bc81dcb3aa89f1b3
Trungggggggggg <97171169+Trungggggggggg@users.noreply.github.com>
2022-01-09 09:11:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng em đã yêu anh từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say anh từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 9h, nhắn tin chơi nha',
+    messDesc: 'Tối nay 8h, nhắn tin chơi nha',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/vothanhtrungggg' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
